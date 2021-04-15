0f29be036efe65003b90066d12dc753fef8d7c31
ian-yitzhak <onyangoian71@gmail.com>
2021-04-15 12:24:30 +0300

    Added files

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '404.html'))
+})
 app.listen(8000, () => console.log('perfect'))
