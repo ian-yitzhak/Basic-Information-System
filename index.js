ba0c8f149f49492e7d4be26ac2bb97372d1cc955
ian-yitzhak <onyangoian71@gmail.com>
2021-04-15 11:57:04 +0300

    Added contact,about,404 pages

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require('path');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
-app.listen(3000, () => console.log('perfect'))
+app.listen(8000, () => console.log('perfect'))
